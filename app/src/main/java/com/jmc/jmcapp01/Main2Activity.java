c83da269a77d17babc860c0981079eb81acaffe9
JMChang168 <postiser@gmail.com>
2016-05-30 16:57:15 +0800

    測試０３

--- app/src/main/java/com/jmc/jmcapp01/Main2Activity.java
+++ app/src/main/java/com/jmc/jmcapp01/Main2Activity.java
@@ -1,10 +1,13 @@
 package com.jmc.jmcapp01;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Main2Activity extends Activity {
 
@@ -14,9 +17,22 @@ public class Main2Activity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main2);
         Button btn_02_01= (Button) findViewById(R.id.button_02_01);
+        final TextView textView1=(TextView) findViewById(R.id.editText_02_01);
+        final TextView textView2=(TextView) findViewById(R.id.editText_02_02);
+
         btn_02_01.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent1=getIntent();
+                String data=intent1.getStringExtra("extra_data");
+                Log.d("SecondActivity", data);
+                textView1.setText(data);
+
+
+                //返回第二頁資料到上一頁
+                Intent intent2=new Intent();
+                intent2.putExtra("data_return",textView2.toString());
+                setResult(RESULT_OK, intent2);
                 finish();
             }
         });
--- app/src/main/java/com/jmc/jmcapp01/MainActivity.java
+++ app/src/main/java/com/jmc/jmcapp01/MainActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -17,16 +19,45 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
-        Button button_01_01=(Button) findViewById(R.id.button_01_01);
+        Button button_01_01 = (Button) findViewById(R.id.button_01_01);
         button_01_01.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                Toast.makeText(MainActivity.this, "開啟第二頁 !!", Toast.LENGTH_SHORT).show();
-                Intent intent=new Intent(Intent.ACTION_VIEW);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse("http://www.google.com.tw"));
                 startActivity(intent);
             }
         });
 
+
+        Button button_01_02 = (Button) findViewById(R.id.button_01_02);
+        button_01_02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = "Hello 第二頁!!";
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                intent.putExtra("extra_data", data);
+                //要求回傳值
+                startActivityForResult(intent, 1);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+
+        TextView textView=(TextView) findViewById(R.id.textView_01_01);
+
+        switch (requestCode){
+            case 1:
+                if(requestCode == RESULT_OK){
+                    String returnedData=data.getStringExtra("data_return");
+                    textView.setText(returnedData);
+                }
+                break;
+            default:
+        }
     }
 }
